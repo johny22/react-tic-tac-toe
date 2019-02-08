e56d75db455d7a29c7048d05f8cb00631f8e0ef8
Jones Romão <jones.rom22@gmail.com>
2019-02-07 23:13:26 -0300

    First steps to history

--- src/index.js
+++ src/index.js
@@ -78,6 +78,14 @@ class Game extends Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
     
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      console.log(desc, step);
+    });
+
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -94,7 +102,7 @@ class Game extends Component {
           />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
